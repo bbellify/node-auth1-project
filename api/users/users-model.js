e5244ba6843b10d3d3eb6dc3d887bbfc19528309
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-13 12:50:02 -0800

    users model built

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,14 +3,17 @@ const db = require('../../data/db-config.js')
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
-async function find() {
-  return db('users')
+function find() {
+
+  return db('users').select('user_id', 'username')
+  
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
+  return db('users').where(filter)
 
 }
 
@@ -18,13 +21,19 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  return db('users')
+    .where('user_id', user_id)
+    .select('user_id', 'username')
+    .first()
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
+async function add(user) {
+  const [id] = await db('users').insert(user)
+
+  return findById(id)
 
 }
 
